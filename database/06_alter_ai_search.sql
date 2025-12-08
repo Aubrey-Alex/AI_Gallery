4ec9f98537fd96751064a5cc37e7f432cfbc8d7e
Aubrey-Alex <1651256706@qq.com>
2025-12-08 17:44:54 +0800

    feat:complete deployment using Docker and Nginx

--- database/06_alter_ai_search.sql
+++ database/06_alter_ai_search.sql
@@ -1,4 +1,4 @@
--- 1. 切换到你的数据库
+-- 1. 切换到数据库
 USE ai_gallery;
 
 -- 2. 修改 image_metadata 表，增加核心向量字段
@@ -7,8 +7,7 @@ USE ai_gallery;
 ALTER TABLE image_metadata 
 ADD COLUMN embedding JSON COMMENT 'AI多模态向量数据(1024维)';
 
--- 3. (可选) 增加一个字段用于标记是否已向量化
--- 这样你的后台任务可以知道哪些图还没处理，方便做“断点续传”
+-- 3. 增加一个字段用于标记是否已向量化
 ALTER TABLE image_metadata 
 ADD COLUMN is_vectorized TINYINT(1) DEFAULT 0 COMMENT '是否已完成向量化: 0-否, 1-是';
 
